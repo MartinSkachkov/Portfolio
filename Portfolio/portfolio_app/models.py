2af4dcb1964773fc03fc26431caa36a1e9483db5
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-29 18:16:46 +0300

    create database model and admin

--- Portfolio/portfolio_app/models.py
+++ Portfolio/portfolio_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+     name = models.CharField(max_length=200)
+     category = models.CharField(max_length=100)
+     github_link = models.URLField(blank=True, null=True)
+     video_link = models.URLField(blank=True, null=True)
+     description = models.TextField()
+
+     def __str__(self):
+        return self.name
