ccb3e65ea998fbf334a3ff6ae9fa5acf77ea4b9a
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-29 18:53:04 +0300

    add layout html template and fix display

--- Portfolio/portfolio_app/urls.py
+++ Portfolio/portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('projects/', views.display_projects, name='projects')
 ]
--- Portfolio/portfolio_app/views.py
+++ Portfolio/portfolio_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'portfolio_app/index.html')
+
+def display_projects(request):
+    return render(request, 'portfolio_app/display_projects.html')
