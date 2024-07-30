778abb6e6ac74e7e8431c9a0d26103f6fe1b8c9a
Martin Skachkov <mgsemailbox@gmail.com>
2024-07-30 17:51:35 +0300

    fix projects display

--- Portfolio/portfolio_app/views.py
+++ Portfolio/portfolio_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def index(request):
     return render(request, 'portfolio_app/index.html')
 
 def display_projects(request):
-    return render(request, 'portfolio_app/display_projects.html')
+    projects = Project.objects.all() # save all entries of the database to a variable
+    print(projects[0].name)
+    context = {'projects':projects}
+    return render(request, 'portfolio_app/display_projects.html', context)
+
+def contact(request):
+    return render(request, 'portfolio_app/contact_me.html')
